270ee0cb70bb07458e507a19d3e6f0a4a5de3515
aodong.qin <ioperations.c@gmail.com>
2022-12-07 23:06:24 +0800

    [rust] [src/leetcode/leetcode_817_linked_list_components.rs] add

---
+++ src/leetcode/leetcode_817_linked_list_components.rs
@@ -0,0 +1,89 @@
+/*
+You are given the head of a linked list containing unique integer values and an integer array nums that is a subset of the linked list values.
+Return the number of connected components in nums where two values are connected if they appear consecutively in the linked list.*/
+
+use std::collections::HashSet;
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode<T> {
+    pub val: T,
+    pub next: Option<Box<ListNode<T>>>,
+}
+
+impl<T> ListNode<T> {
+    #[inline]
+    fn new(val: T) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+#[allow(unused)]
+struct Solution;
+
+impl Solution {
+    #[allow(unused)]
+    pub fn num_components(head: Option<Box<ListNode<i32>>>, nums: Vec<i32>) -> i32 {
+        let nums: HashSet<_> = nums.into_iter().collect();
+        (0..)
+            .scan(&head, |node, _| {
+                node.as_deref().map(|ListNode { val, next }| {
+                    *node = next;
+                    nums.contains(val)
+                })
+            })
+            .fuse()
+            .fold((0, false), |(cnt, pre), b| (i32::from(b && !pre) + cnt, b))
+            .0
+    }
+}
+
+#[allow(unused)]
+fn list_into_vec(head: &Option<Box<ListNode<i32>>>) -> Vec<i32> {
+    if let Some(h) = head {
+        let mut v = list_into_vec(&h.next);
+        v.insert(0, h.val);
+        return v;
+    }
+
+    vec![]
+}
+
+#[allow(unused)]
+fn build_list_from_vec(vec: &[i32]) -> Option<Box<ListNode<i32>>> {
+    if vec.is_empty() {
+        return None;
+    }
+    let ret = build_list_from_vec(&vec[1..]);
+    let mut head = Box::new(ListNode::new(vec[0]));
+    head.next = ret;
+
+    Some(head)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1_test() {
+        let head = [0, 1, 2, 3];
+        let nums = [0, 1, 3];
+        let output = 2;
+        // Explanation: 0 and 1 are connected, so [0, 1] and [3] are the two connected components.
+        let link_list = build_list_from_vec(&head);
+        let ret = Solution::num_components(link_list, nums.into());
+        assert_eq!(ret, output);
+    }
+
+    #[test]
+    fn case2_test() {
+        let head = [0, 1, 2, 3, 4];
+        let nums = [0, 3, 1, 4];
+        let output = 2;
+        // 0 and 1 are connected, 3 and 4 are connected, so [0, 1] and [3, 4] are the two connected components.
+        let link_list = build_list_from_vec(&head);
+
+        let ret = Solution::num_components(link_list, nums.into());
+        assert_eq!(ret, output);
+    }
+}
