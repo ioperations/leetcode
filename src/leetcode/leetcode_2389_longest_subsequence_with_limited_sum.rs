923372a74fe346336c30c060f24a587eb07bee53
aodong.qin <ioperations.c@gmail.com>
2022-12-25 09:31:04 +0800

    [rust] [src/leetcode/leetcode_2389_longest_subsequence_with_limited_sum.rs] add

---
+++ src/leetcode/leetcode_2389_longest_subsequence_with_limited_sum.rs
@@ -0,0 +1,62 @@
+/*
+You are given an integer array nums of length n, and an integer array queries of length m.
+Return an array answer of length m where answer[i] is the maximum size of a subsequence that you can take from nums such that the sum of its elements is less than or equal to queries[i].
+A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+*/
+struct Solution;
+impl Solution {
+    fn answer_queries(nums: Vec<i32>, queries: Vec<i32>) -> Vec<i32> {
+        let len_ns: usize = nums.len();
+        let len_qs: usize = queries.len();
+        let nums: Vec<i32> = {
+            let mut nums = nums;
+            nums.sort();
+            nums
+        };
+        let mut ans: Vec<i32> = vec![0; len_qs];
+        for (idx_q, query) in queries.into_iter().enumerate() {
+            let mut sum = 0;
+            for idx_n in 0..len_ns {
+                sum += nums[idx_n];
+                if sum > query {
+                    ans[idx_q] = idx_n as i32;
+                    break;
+                } else {
+                    ans[idx_q] = idx_n as i32 + 1;
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1_test() {
+        let nums = [4, 5, 2, 1];
+        let queries = [3, 10, 21];
+        let output = [2, 3, 4];
+        /* Explanation: We answer the queries as follows:
+        - The subsequence [2,1] has a sum less than or equal to 3. It can be proven that 2 is the maximum size of such a subsequence, so answer[0] = 2.
+        - The subsequence [4,5,1] has a sum less than or equal to 10. It can be proven that 3 is the maximum size of such a subsequence, so answer[1] = 3.
+        - The subsequence [4,5,2,1] has a sum less than or equal to 21. It can be proven that 4 is the maximum size of such a subsequence, so answer[2] = 4.
+        */
+        let ret = Solution::answer_queries(nums.into(), queries.into());
+        assert_eq!(ret, output);
+    }
+
+    #[test]
+    fn case2_test() {
+        let nums = [2, 3, 4, 5];
+        let queries = [1];
+        let output = [0];
+        /*
+        The empty subsequence is the only subsequence that has a sum less than or equal to 1, so answer[0] = 0.
+        */
+        let ret = Solution::answer_queries(nums.into(), queries.into());
+        assert_eq!(ret, output);
+    }
+}
