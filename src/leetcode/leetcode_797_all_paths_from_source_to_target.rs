143aa4e0f9cda396698d9f6bcf4e7178e79eb393
aodong.qin <ioperations.c@gmail.com>
2022-12-31 18:07:45 +0000

    [rust] [src/leetcode/leetcode_980_unique_paths_iii.rs] #backtrack #dfs

---
+++ src/leetcode/leetcode_797_all_paths_from_source_to_target.rs
@@ -0,0 +1,78 @@
+/*Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1, find all possible paths from node 0 to node n - 1 and return them in any order.↳
+
+The graph is given as follows: graph[i] is a list of all nodes you can visit from node i (i.e., there is a directed edge from node i to node graph[i][j]).
+*/
+
+#[allow(unused)]
+struct Solution;
+impl Solution {
+    #[allow(unused)]
+    fn dfs(
+        graph: &[Vec<i32>],
+        node: i32,
+        n: i32,
+        path: &mut Vec<i32>,
+        mut paths: Vec<Vec<i32>>,
+    ) -> Vec<Vec<i32>> {
+        path.push(node);
+
+        if node == n {
+            // We have reached the target. Append path to output
+            paths.push(path.clone());
+        } else {
+            // Branch out to all children
+            for child in &graph[node as usize] {
+                paths = Self::dfs(graph, *child, n, path, paths);
+            }
+        }
+
+        // backtrack
+        path.pop();
+        paths
+    }
+
+    #[allow(unused)]
+    pub fn all_paths_source_target(graph: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        Self::dfs(&graph, 0, (graph.len() - 1) as i32, &mut vec![], vec![])
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashSet;
+
+    use super::*;
+
+    #[test]
+    fn case1_test() {
+        let graph = vec![vec![1, 2], vec![3], vec![3], vec![]];
+        let output = vec![vec![0, 1, 3], vec![0, 2, 3]]
+            .into_iter()
+            .collect::<HashSet<Vec<i32>>>();
+        //Explanation: There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+        let ret: HashSet<Vec<i32>> = Solution::all_paths_source_target(graph)
+            .into_iter()
+            .collect();
+        assert_eq!(ret, output);
+    }
+
+    #[test]
+    fn case2_test() {
+        let graph = vec![vec![4, 3, 1], vec![3, 2, 4], vec![3], vec![4], vec![]];
+        let output = vec![
+            vec![0, 4],
+            vec![0, 3, 4],
+            vec![0, 1, 3, 4],
+            vec![0, 1, 2, 3, 4],
+            vec![0, 1, 4],
+        ]
+        .into_iter()
+        .collect::<HashSet<Vec<i32>>>();
+        //Explanation: There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+        let ret: HashSet<Vec<i32>> = Solution::all_paths_source_target(graph)
+            .into_iter()
+            .collect();
+
+        assert_eq!(ret, output);
+    }
+}
---
+++ src/leetcode/leetcode_980_unique_paths_iii.rs
@@ -0,0 +1,119 @@
+/*You are given an m x n integer array grid where grid[i][j] could be:
+1 representing the starting square. There is exactly one starting square.
+2 representing the ending square. There is exactly one ending square.
+0 representing empty squares we can walk over.
+-1 representing obstacles that we cannot walk over.
+Return the number of 4-directional walks from the starting square to the ending square, that walk over every non-obstacle square exactly once.
+*/
+
+use std::collections::HashSet;
+
+#[allow(unused)]
+struct Solution;
+impl Solution {
+    #[allow(unused)]
+    pub fn unique_paths_iii(grid: Vec<Vec<i32>>) -> i32 {
+        let m = grid.len();
+        let n = grid[0].len();
+        let cur = (|| {
+            for i in 0..m {
+                for j in 0..n {
+                    if grid[i][j] == 1 {
+                        return (i, j);
+                    }
+                }
+            }
+            panic!();
+        })();
+        let target = {
+            let mut res = 0;
+            for i in 0..m {
+                for j in 0..n {
+                    if grid[i][j] == 0 {
+                        res += 1;
+                    }
+                }
+            }
+            res + 1
+        };
+        let mut route = HashSet::new();
+        Self::dfs(&grid, (m - 1, n - 1), cur, target, &mut route)
+    }
+
+    #[allow(unused)]
+    fn dfs(
+        grid: &[Vec<i32>],
+        bounds: (usize, usize),
+        cur: (usize, usize),
+        target: usize,
+        route: &mut HashSet<(usize, usize)>,
+    ) -> i32 {
+        let (m, n) = bounds;
+        let (i, j) = cur;
+
+        if grid[i][j] == 2 && route.len() == target {
+            return 1;
+        }
+        route.insert(cur);
+        let mut try_dfs = |pos: (usize, usize)| {
+            if grid[i][j] == -1 || route.contains(&pos) {
+                return 0;
+            };
+            return Self::dfs(grid, bounds, pos, target, route);
+        };
+        let mut res = 0;
+        if i > 0 {
+            res += try_dfs((i - 1, j));
+        }
+        if i < m {
+            res += try_dfs((i + 1, j));
+        }
+        if j > 0 {
+            res += try_dfs((i, j - 1));
+        }
+        if j < n {
+            res += try_dfs((i, j + 1));
+        }
+        route.remove(&cur);
+        res
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1_test() {
+        let grid = vec![vec![1, 0, 0, 0], vec![0, 0, 0, 0], vec![0, 0, 2, -1]];
+        let output = 2;
+        // Explanation: We have the following two paths:
+        // 1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2)
+        // 2. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2)
+        let ret = Solution::unique_paths_iii(grid);
+        assert_eq!(ret, output);
+    }
+
+    #[test]
+    fn case2_test() {
+        let grid = vec![vec![1, 0, 0, 0], vec![0, 0, 0, 0], vec![0, 0, 0, 2]];
+        let output = 4;
+        //          We have the following four paths:
+        // 1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2),(2,3)
+        // 2. (0,0),(0,1),(1,1),(1,0),(2,0),(2,1),(2,2),(1,2),(0,2),(0,3),(1,3),(2,3)
+        // 3. (0,0),(1,0),(2,0),(2,1),(2,2),(1,2),(1,1),(0,1),(0,2),(0,3),(1,3),(2,3)
+        // 4. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2),(2,3)
+        let ret = Solution::unique_paths_iii(grid);
+        assert_eq!(ret, output);
+    }
+
+    #[test]
+    fn case3_test() {
+        let grid = vec![vec![0, 1], vec![2, 0]];
+        let output = 0;
+        // There is no path that walks over every empty square exactly once.
+        // Note that the starting and ending square can be anywhere in the grid.
+        let ret = Solution::unique_paths_iii(grid);
+        assert_eq!(ret, output);
+    }
+}
