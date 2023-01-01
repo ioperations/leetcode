08348d669e1049baeaa48aaefdd4969691c8ffab
aodong.qin <ioperations.c@gmail.com>
2023-01-01 11:39:57 +0000

    [rust] [src/leetcode/leetcode_290_word_pattern.rs] #bijection_map

---
+++ src/leetcode/leetcode_290_word_pattern.rs
@@ -0,0 +1,66 @@
+/*Given a pattern and a string s, find if s follows the same pattern.
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s. */
+
+#[allow(unused)]
+struct Solution;
+impl Solution {
+    #[allow(unused)]
+    pub fn word_pattern(pattern: String, s: String) -> bool {
+        use std::collections::HashMap;
+        use std::collections::HashSet;
+
+        let mut bijection_mapping: HashMap<char, &str> = HashMap::new();
+        let mut words: Vec<&str> = s.split_whitespace().collect();
+        let word_set: HashSet<&str> = words.iter().cloned().collect();
+
+        if pattern.len() != words.len() {
+            return false;
+        }
+
+        for (i, c) in pattern.chars().enumerate() {
+            if let Some(value) = bijection_mapping.get(&c) {
+                if value != &words[i] {
+                    return false;
+                }
+            }
+            bijection_mapping.insert(c, words[i]);
+        }
+        if bijection_mapping.len() != word_set.len() {
+            false
+        } else {
+            true
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1_test() {
+        let pattern = "abba";
+        let s = "dog cat cat dog";
+        let output = true;
+        let ret = Solution::word_pattern(pattern.into(), s.into());
+        assert_eq!(ret, output);
+    }
+
+    #[test]
+    fn case2_test() {
+        let pattern = "abba";
+        let s = "dog cat cat fish";
+        let output = false;
+        let ret = Solution::word_pattern(pattern.into(), s.into());
+        assert_eq!(ret, output);
+    }
+
+    #[test]
+    fn case4_test() {
+        let pattern = "aaaa";
+        let s = "dog cat cat dog";
+        let output = false;
+        let ret = Solution::word_pattern(pattern.into(), s.into());
+        assert_eq!(ret, output);
+    }
+}
