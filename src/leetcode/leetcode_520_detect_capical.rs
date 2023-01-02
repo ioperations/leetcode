92ca79342e0633700da79b28e659cb8391b5d345
aodong.qin <ioperations.c@gmail.com>
2023-01-02 10:59:49 +0000

    [rust] [src/leetcode/leetcode_520_detect_capical.rs] add #const generatic

---
+++ src/leetcode/leetcode_520_detect_capical.rs
@@ -0,0 +1,48 @@
+/*
+We define the usage of capitals in a word to be right when one of the following cases holds:
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Given a string word, return true if the usage of capitals in it is right.*/
+
+#[allow(unused)]
+struct Solution;
+impl Solution {
+    #[allow(unused)]
+    pub fn detect_capital_use(word: String) -> bool {
+        fn all_upper<const T: bool>(words: &[char]) -> bool {
+            if T {
+                words.iter().all(|&c| ('a'..='z').contains(&c))
+            } else {
+                words.iter().all(|&c| ('A'..='Z').contains(&c))
+            }
+        }
+        if word.is_empty() || word.len() == 1 {
+            return true;
+        }
+
+        let word = word.chars().skip(1).collect::<Vec<char>>();
+        all_upper::<true>(&word) || all_upper::<false>(&word)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1_test() {
+        let word = "USA";
+        let output = true;
+        let ret = Solution::detect_capital_use(word.into());
+        assert_eq!(ret, output);
+    }
+
+    #[test]
+    fn case2_test() {
+        let word = "FlaG";
+        let output = false;
+        let ret = Solution::detect_capital_use(word.into());
+        assert_eq!(ret, output);
+    }
+}
