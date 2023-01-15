5cd8b4a8d5f4d5ea36196e098c0ffc0527dba582
aodong.qin <ioperations.c@gmail.com>
2023-01-15 12:59:31 +0800

    [rust] [src/leetcode/leetcode_19_remove_nth_node_from_end_of_list.rs] add

---
+++ src/leetcode/leetcode_19_remove_nth_node_from_end_of_list.rs
@@ -0,0 +1,82 @@
+// Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+use super::leetcode_linklist::ListNode;
+#[allow(unused)]
+struct Solution;
+
+impl Solution {
+    #[allow(unused)]
+    pub fn remove_nth_from_end<T>(
+        head: Option<Box<ListNode<T>>>,
+        n: i32,
+    ) -> Option<Box<ListNode<T>>>
+    where
+        T: Copy + Default,
+    {
+        let mut dummy = Box::new(ListNode::<T> {
+            val: Default::default(),
+            next: head,
+        });
+
+        unsafe {
+            let mut slow = &mut dummy as *mut Box<ListNode<T>>;
+
+            // miri 报错 error: Undefined Behavior
+            // let mut fast = &mut dummy as *mut Box<ListNode>;
+            let mut fast = &dummy as *const Box<ListNode<T>>;
+
+            for _ in 0..n {
+                // fast = (*fast).next.as_mut().unwrap();
+                fast = (*fast).next.as_ref().unwrap();
+            }
+
+            while (*fast).next.is_some() {
+                // fast = (*fast).next.as_mut().unwrap();
+                fast = (*fast).next.as_ref().unwrap();
+
+                slow = (*slow).next.as_mut().unwrap();
+            }
+            (*slow).next = (*slow).next.take().unwrap().next;
+        }
+        dummy.next
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::leetcode_linklist::{build_list_from_vec, list_into_vec};
+    use super::*;
+
+    #[test]
+    fn case1_test() {
+        let head = [1, 2, 3, 4, 5];
+        let n = 2;
+        let output = [1, 2, 3, 5];
+        let root = build_list_from_vec(&head);
+        let root = Solution::remove_nth_from_end(root, n);
+        let ret = list_into_vec(&root);
+        assert_eq!(ret, output);
+    }
+
+    #[test]
+    fn case2_test() {
+        let head = [1];
+        let n = 1;
+        let output = [];
+        let root = build_list_from_vec(&head);
+        let root = Solution::remove_nth_from_end(root, n);
+        let ret = list_into_vec(&root);
+        assert_eq!(ret, output);
+    }
+
+    #[test]
+    fn case3_test() {
+        let head = [1, 2];
+        let n = 1;
+        let output = [1];
+        let root = build_list_from_vec(&head);
+        let root = Solution::remove_nth_from_end(root, n);
+        let ret = list_into_vec(&root);
+        assert_eq!(ret, output);
+    }
+}
