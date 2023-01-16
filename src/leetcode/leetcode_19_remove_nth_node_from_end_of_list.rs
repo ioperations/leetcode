56efdd58b7a38954669da4dc14d635b906a19c9c
aodong.qin <ioperations.c@gmail.com>
2023-01-17 07:59:42 +0800

    [rust] [src/leetcode/leetcode_19_remove_nth_node_from_end_of_list.rs] #fast slow pointer with unsafe code

--- src/leetcode/leetcode_19_remove_nth_node_from_end_of_list.rs
+++ src/leetcode/leetcode_19_remove_nth_node_from_end_of_list.rs
@@ -41,6 +41,35 @@ impl Solution {
         }
         dummy.next
     }
+
+    #[allow(unused)]
+    #[allow(clippy::borrow_as_ptr)]
+    pub fn remove_middle_node<T>(head: Option<Box<ListNode<T>>>) -> Option<Box<ListNode<T>>>
+    where
+        T: Copy + Default,
+    {
+        let mut dummy = Box::new(ListNode::<T> {
+            val: Default::default(),
+            next: head,
+        });
+
+        unsafe {
+            let mut fast = &dummy as *const Box<ListNode<T>>;
+            let mut slower = &mut dummy as *mut Box<ListNode<T>>;
+
+            while (*fast).next.is_some() {
+                fast = (*fast).next.as_ref().unwrap();
+                if (*fast).next.is_some() {
+                    fast = (*fast).next.as_ref().unwrap();
+                } else {
+                    break;
+                }
+                slower = (*slower).next.as_mut().unwrap();
+            }
+            (*slower).next = (*slower).next.take().unwrap().next;
+        }
+        dummy.next
+    }
 }
 
 #[cfg(test)]
@@ -77,6 +106,42 @@ mod tests {
         let output = [1];
         let root = build_list_from_vec(&head);
         let root = Solution::remove_nth_from_end(root, n);
+        let ret = list_into_vec(&root);
+        assert_eq!(ret, output);
+    }
+}
+
+#[cfg(test)]
+mod tests_middle_node {
+    use super::super::leetcode_linklist::{build_list_from_vec, list_into_vec};
+    use super::*;
+
+    #[test]
+    fn case1_test() {
+        let head = [1, 2, 3, 4, 5];
+        let output = [1, 2, 4, 5];
+        let root = build_list_from_vec(&head);
+        let root = Solution::remove_middle_node(root);
+        let ret = list_into_vec(&root);
+        assert_eq!(ret, output);
+    }
+
+    #[test]
+    fn case2_test() {
+        let head = [1];
+        let output = [];
+        let root = build_list_from_vec(&head);
+        let root = Solution::remove_middle_node(root);
+        let ret = list_into_vec(&root);
+        assert_eq!(ret, output);
+    }
+
+    #[test]
+    fn case3_test() {
+        let head = [1, 2];
+        let output = [1];
+        let root = build_list_from_vec(&head);
+        let root = Solution::remove_middle_node(root);
         let ret = list_into_vec(&root);
         assert_eq!(ret, output);
     }
