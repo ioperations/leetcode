f8d673fe7e0ce64ca1b3862d2237d221fd49fa84
aodong.qin <ioperations.c@gmail.com>
2023-09-16 09:46:08 +0800

    accept refactor suggestion

--- src/classic/char_size.rs
+++ src/classic/char_size.rs
@@ -4,12 +4,12 @@ mod tests {
     #[test]
     fn char_size_test() {
         let z = 'c';
-        assert!(std::mem::size_of_val(&z) == 4);
+        assert_eq!(std::mem::size_of_val(&z), 4);
     }
 
     #[test]
     fn list_usage_v2() {
         let guo = '国';
-        assert!(std::mem::size_of_val(&guo) == 4);
+        assert_eq!(std::mem::size_of_val(&guo), 4);
     }
 }
