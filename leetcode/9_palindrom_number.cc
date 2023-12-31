2c85b8f67c8ccd6217d6cf8f7ec74a458d025275
aodong.qin <ioperations.c@gmail.com>
2023-12-31 17:00:11 +0800

    fix typo

--- leetcode/9_palindrom_number.cc
+++ leetcode/9_palindrom_number.cc
@@ -4,10 +4,10 @@
 
 #include <string>
 
-class Sulution {
+class Solution {
    public:
-    Sulution(){};
-    virtual ~Sulution(){};
+    Solution(){};
+    virtual ~Solution(){};
     bool IsPalindrome(int x) {
         return std::to_string(x) == Reverse(std::to_string(x));
     }
@@ -27,23 +27,24 @@ class Sulution {
 #include <iostream>
 
 TEST(t0, t1) {
-    Sulution s;
+    Solution s;
 
     bool ret = s.IsPalindrome(121);
     EXPECT_EQ(ret, true);
 }
 
 TEST(t0, t2) {
-    Sulution s;
+    Solution s;
 
     bool ret = s.IsPalindrome(-121);
     EXPECT_EQ(ret, false);
 }
 
 TEST(t0, t3) {
-    Sulution s;
+    Solution s;
 
     bool ret = s.IsPalindrome(10);
+
     EXPECT_EQ(ret, false);
 }
 int main(int argc, char *argv[]) {
