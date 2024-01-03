379020b74dd0d4162c07fbb274a7941837c43246
aodong.qin <ioperations.c@gmail.com>
2024-01-03 11:05:29 +0800

    update stackage-snapshot version

--- hs/src/Lib.hs
+++ hs/src/Lib.hs
@@ -11,8 +11,11 @@ someFunc = print (surface (Circle (Point 1 2) 3))
 
 data Point = Point {x :: !Float, y :: !Float} deriving (Show)
 
+-- a Graph is a Circle or a Rentangle
 data MyCricle = Circle {point :: !Point, len :: !Float} | Rectangle {left_bellow :: !Point, right_up :: !Point} deriving (Show)
 
+--- >>> (surface (Circle (Point 1 2 ) 3 ))
+-- 28.26
 surface :: MyCricle -> Float
 surface (Circle _ a) = pi * a ^ 2
   where
