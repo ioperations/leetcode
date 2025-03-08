7f5d79a63036ab5a2040b2ae5a4a2945f1e9597c
aodong.qin <ioperations.c@gmail.com>
2025-03-08 21:23:29 +0800

    [cc] cc/2379_minimum_recolors_to_get_k_Consecutive_black_blocks.cc

--- cc/2379_minimum_recolors_to_get_k_Consecutive_black_blocks.cc
+++ cc/2379_minimum_recolors_to_get_k_Consecutive_black_blocks.cc
@@ -1,19 +1,19 @@
 /*
  * You are given a 0-indexed string blocks of length n, where blocks[i] is
-either 'W' or 'B', representing the color of the ith block. The characters 'W'
-and 'B' denote the colors white and black, respectively.
+ * either 'W' or 'B', representing the color of the ith block. The characters
+ * 'W' and 'B' denote the colors white and black, respectively.
+ *
+ * You are also given an integer k, which is the desired number of consecutive
+ * black blocks.
+ *
+ * In one operation, you can recolor a white block such that it becomes a black
+ * block.
+ *
+ * Return the minimum number of operations needed such that there is at least
+ * one occurrence of k consecutive black blocks.
+ *
+ */
 
-You are also given an integer k, which is the desired number of consecutive
-black blocks.
-
-In one operation, you can recolor a white block such that it becomes a black
-block.
-
-Return the minimum number of operations needed such that there is at least one
-occurrence of k consecutive black blocks.
-
-
- * */
 #include <algorithm>
 #include <string>
 using namespace std;
@@ -26,12 +26,10 @@ class Solution {
          * 1 <= n <= 100
          * blocks[i] is either 'W' or 'B'.
          * 1 <= k <= n
-         *
          */
 
         auto size = blocks.size();
         int whiteSize = 0;
-
         for (int i = 0; i < k; i++) {
             if (blocks[i] == 'W') {
                 whiteSize++;
