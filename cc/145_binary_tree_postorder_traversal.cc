85b02e3fd971726f2b38cce786130de38ea4b6d6
aodong.qin <ioperations.c@gmail.com>
2025-05-02 11:46:29 +0800

    [cc] refactor

--- cc/145_binary_tree_postorder_traversal.cc
+++ cc/145_binary_tree_postorder_traversal.cc
@@ -13,14 +13,14 @@ namespace {
 class Solution {
    public:
     template <typename T>
-    std::vector<int> PostorderTraversal(TreeNode<T> *root) {
-        std::vector<int> ret;
+    std::vector<T> PostorderTraversal(TreeNode<T>* root) {
+        std::vector<T> ret;
         Tranverse(root, ret);
         return ret;
     }
 
     template <typename T>
-    void Tranverse(TreeNode<T> *root, std::vector<int> &val) {
+    void Tranverse(TreeNode<T>* root, std::vector<T>& val) {
         if (root == nullptr) {
             return;
         }
@@ -33,16 +33,16 @@ class Solution {
 };
 
 TEST(binary_tree_postorder_traversal, t1) {
-    TreeNode<int> *root = ConstructTree(std::vector<int>{1, 2, 3});
+    TreeNode<int>* root = ConstructTree(std::vector{1, 2, 3});
     Solution s;
     std::vector<int> ret = s.PostorderTraversal(root);
 
-    EXPECT_EQ(ret, (std::vector<int>{3, 2, 1}));
+    EXPECT_EQ(ret, (std::vector{3, 2, 1}));
 
     FreeTreeNode(root);
 }
 TEST(binary_tree_postorder_traversal, t2) {
-    TreeNode<int> *root = ConstructTree(std::vector<int>{});
+    TreeNode<int>* root = ConstructTree(std::vector<int>{});
     Solution s;
     std::vector<int> ret = s.PostorderTraversal(root);
 
@@ -51,11 +51,11 @@ TEST(binary_tree_postorder_traversal, t2) {
     FreeTreeNode(root);
 }
 TEST(binary_tree_postorder_traversal, t3) {
-    TreeNode<int> *root = ConstructTree(std::vector<int>{1});
+    TreeNode<int>* root = ConstructTree(std::vector{1});
     Solution s;
     std::vector<int> ret = s.PostorderTraversal(root);
 
-    EXPECT_EQ(ret, std::vector<int>{1});
+    EXPECT_EQ(ret, std::vector{1});
     FreeTreeNode(root);
 }
 
