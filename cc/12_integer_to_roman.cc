f5b61b43b603aca29f955aca740fabe26b4b7689
aodong.qin <ioperations.c@gmail.com>
2025-06-06 00:16:46 +0800

    [cc] fix

--- cc/12_integer_to_roman.cc
+++ cc/12_integer_to_roman.cc
@@ -44,13 +44,13 @@ class Solution {
         int const normal[] = {1000, 900, 500, 400, 100, 90, 50,
                               40,   10,  9,   5,   4,   1};
         int const size = sizeof(normal) / sizeof(int);
-        static const std::array<std::string, 13> roman[] = {
+        static const std::array<std::string, 13> roman = {
             "M",  "CM", "D",  "CD", "C",  "XC", "L",
             "XL", "X",  "IX", "V",  "IV", "I"};
         string res;
         for (int i = 0; i < size; i++) {
             while (num >= normal[i]) {
-                res.push_back(roman[i]);
+                res.append(roman[i]);
                 num -= normal[i];
             }
         }
