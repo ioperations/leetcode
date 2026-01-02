aad95055dc7a8360fdf7a65ace9b633b1ba6447c
aodong.覃奥东 <ioperations.c@gmail.com>
2026-01-02 11:57:27 +0800

    [for_each] fix

--- cc/for_each.hh
+++ cc/for_each.hh
@@ -4,19 +4,21 @@
 #include <type_traits>
 #include <utility>
 
-using namespace std;
-
 template <typename T, typename = void>
 struct IsIterator : std::false_type {};
 
 template <typename T>
 struct IsIterator<T, std::void_t<decltype(std::begin(std::declval<T&>())),
-                                 decltype(std::end(std::declval<T&>()))>>
+                                 decltype(std::end(std::declval<T&>())),
+                                 std::enable_if_t<!std::is_same_v<std::decay_t<T>, std::string>>>>
     : std::true_type {};
 
 template <typename T, typename Func>
 void DfsForEach(T&& list_or_item, Func func) {
-    if constexpr (IsIterator<T>::value || !std::is_same_v<std::string, T>) {
+    if constexpr (IsIterator<T>::value) {
+        // 使用 std::begin 确保在没有 ADL 的情况下也能找到定义
+        using std::begin;
+        using std::end;
         for (auto& item : list_or_item) {
             DfsForEach(item, func);
         }
