72c9522176ed665549bfa5ba02ce110dcc215849
aodong.qin <ioperations.c@gmail.com>
2026-06-06 15:11:35 +0800

    limit vscode related process

--- scripts/limit_vscode_cpu.sh
+++ scripts/limit_vscode_cpu.sh
@@ -1,9 +1,7 @@
-#!/usr/bin/env bash
+#/usr/bin/bash
 
 mkdir -p /sys/fs/cgroup/vscode_limit/
-echo "800000 100000" >/sys/fs/cgroup/vscode_limit/cpu.max
-
-mainId=$(ps axf | grep /opt/visual-studio-code/code | grep -v grep | head -n 1 | awk '{print $1}')
+echo "800000 100000" > /sys/fs/cgroup/vscode_limit/cpu.max
 
 get_all_subprocesses() {
     local ppid="$1"
@@ -12,14 +10,25 @@ get_all_subprocesses() {
 
     for pid in $sub_pids; do
         echo "$pid"
+        # 递归调用
         get_all_subprocesses "$pid"
     done
 }
 
-parent_pid=$mainId
-my_pid_vector=($(get_all_subprocesses "$parent_pid"))
+echo "" > /sys/fs/cgroup/vscode_limit/cgroup.procs
 
-echo "" >/sys/fs/cgroup/vscode_limit/cgroup.procs
-for i in "${my_pid_vector[@]}"; do
-    echo "$i" | tee -a /sys/fs/cgroup/vscode_limit/cgroup.procs
+mainId2=$(ps axf |egrep -e 'clangd|ccls|vscode' |grep -v grep | awk '{print $1}')
+DELIMITER="\n"
+
+# 2. Split the string into an array named 'MY_ARRAY'
+IFS="$DELIMITER" read -r -a MY_ARRAY <<< "$mainId2"
+
+for p in "${MY_ARRAY[@]}"; do 
+    parent_pid=$p # 替换为您的实际父进程 PID
+    echo $parent_pid | tee -a /sys/fs/cgroup/vscode_limit/cgroup.procs
+    my_pid_vector=( $(get_all_subprocesses "$parent_pid") )
+    for i in "${my_pid_vector[@]}"; do
+        echo $i | tee -a /sys/fs/cgroup/vscode_limit/cgroup.procs
+    done
 done
+
