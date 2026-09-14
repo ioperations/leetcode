cb0979a090c0931c1431d5640345637777081eab
aodong.qin <ioperations.c@gmail.com>
2026-09-15 00:39:43 +0800

    [cc] [cc/2265_count_nodes_equal_to_average_of_subtree.cc] free

--- cc/2265_count_nodes_equal_to_average_of_subtree.cc
+++ cc/2265_count_nodes_equal_to_average_of_subtree.cc
@@ -63,11 +63,13 @@ TEST(AverageOfSubtree, t1) {
     Solution sl;
     int ret = sl.AverageOfSubtree(tree);
     EXPECT_EQ(output, ret);
-    /*For the node with value 4: The average of its subtree is (4 + 8 + 5 + 0 +
-1 + 6) / 6 = 24 / 6 = 4. For the node with value 5: The average of its subtree
-is (5 + 6) / 2 = 11 / 2 = 5. For the node with value 0: The average of its
-subtree is 0 / 1 = 0. For the node with value 1: The average of its subtree is 1
-/ 1 = 1. For the node with value 6: The average of its subtree is 6 / 1 = 6.*/
+    Tree::FreeTreeNode(tree);
+    /* For the node with value 4: The average of its subtree is (4 + 8 + 5 + 0 +
+     * 1 + 6) / 6 = 24 / 6 = 4. For the node with value 5: The average of its
+     * subtree is (5 + 6) / 2 = 11 / 2 = 5. For the node with value 0: The
+     * average of its subtree is 0 / 1 = 0. For the node with value 1: The
+     * average of its subtree is 1 / 1 = 1. For the node with value 6: The
+     * average of its subtree is 6 / 1 = 6.*/
 }
 
 TEST(AverageOfSubtree, t2) {
@@ -77,6 +79,7 @@ TEST(AverageOfSubtree, t2) {
     Solution sl;
     int ret = sl.AverageOfSubtree(tree);
     EXPECT_EQ(output, ret);
+    Tree::FreeTreeNode(tree);
 }
 
 int main(int argc, char* argv[]) {
